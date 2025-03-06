38da788733fd8ac4de60b44ab40d1f308d33f102
initcatcher <initcatcher@gmail.com>
2025-03-06 15:39:34 +0900

    [Gold IV] Title: 알파벳, Time: 796 ms, Memory: 12236 KB -BaekjoonHub

--- Java/백준/Gold/1987. 알파벳/알파벳.java
+++ Java/백준/Gold/1987. 알파벳/알파벳.java
@@ -2,97 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static StringTokenizer st;
-
-    static void bwEnd() throws IOException {bw.flush();bw.close();}
-
-    static void readLine() throws IOException {
-        st = new StringTokenizer(br.readLine());
-    }
-
-    static int nextInt(){
-        return Integer.parseInt(st.nextToken());
-    }
-
-    static long nextLong(){
-        return Long.parseLong(st.nextToken());
-    }
-
-    static int[][] grid = new int[20][20];
-    static int R,C;
-
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    
+    static int R, C;
+    static char[][] board;
     static int ans = 0;
-
-    static int[] dy = new int[]{-1,1,0,0};
-    static int[] dx = new int[]{0,0,-1,1};
-
-
-    static boolean isOk(int y, int x){
-        return 0<=y && y<R && 0<=x && x<C;
+    
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    
+    static boolean isOk(int y, int x) {
+        return y >= 0 && y < R && x >= 0 && x < C;
     }
-
-    static class Horse {
-        int y;
-        int x;
-        int mask;
-
-        Horse(int y, int x, int mask){
-            this.y = y;
-            this.x = x;
-            this.mask = mask;
-        }
-    }
-
-    static int getCur(int mask){
-        int count = 0;
-        for (int i = 0; i < 26; i++) 
-            if((mask & 1<<i) > 0) count++;
-        return count;
-    }
-
-
-    static void solve(){
+    
+    static void dfs(int y, int x, int mask, int count) {
+        ans = Math.max(ans, count);
         
-        HashMap<String,Boolean> map = new HashMap<>(); 
-        Queue<Horse> queue = new LinkedList<>();
-        queue.offer(new Horse(0,0, 1<<grid[0][0]));
-        while(!queue.isEmpty()){
-            Horse cur = queue.poll();
-
-            ans = Math.max(ans, getCur(cur.mask));
-
-            for (int k = 0; k < 4; k++) {
-                int nextY = cur.y + dy[k];
-                int nextX = cur.x + dx[k];
-
-
-                if(isOk(nextY, nextX) && (cur.mask & 1<<grid[nextY][nextX]) == 0){
-                    int newMask = cur.mask | 1<< grid[nextY][nextX];
-                    String key = new String("" +nextY + " " +nextX +newMask);
-                    if(map.containsKey(key)) continue;
-                    map.put(key,true);
-                    queue.offer(new Horse(nextY, nextX, newMask));
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            
+            if (isOk(ny, nx)) {
+                int letter = board[ny][nx] - 'A';
+                if ((mask & (1 << letter)) == 0) {  // 아직 방문하지 않은 알파벳이면
+                    dfs(ny, nx, mask | (1 << letter), count + 1);
                 }
             }
         }
     }
-
+    
     public static void main(String[] args) throws Exception {
-        readLine();
-        R= nextInt();
-        C = nextInt();
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
         for (int i = 0; i < R; i++){
-            char[] tmp = br.readLine().toCharArray();
-            for (int j = 0; j < C; j++)
-                grid[i][j] = tmp[j] - 'A';
+            board[i] = br.readLine().toCharArray();
         }
-        
-        solve();
-        
-
-        bw.write(String.format("%d\n",ans));
-        bwEnd();
+        int initialMask = 1 << (board[0][0] - 'A');
+        dfs(0, 0, initialMask, 1);
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
     }
 }
