b9afd57db9e3386510da272f28d62a606bc3e636
initcatcher <initcatcher@gmail.com>
2025-08-27 11:21:25 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    const answer = [n]
+    
+    let num = n
+    while (num !== 1){
+        if(num % 2 === 0){
+            num = (num/2 |0)
+        }else{
+            num =3*num+1
+        }
+        answer.push(num)
+    }
+    return answer
+}
