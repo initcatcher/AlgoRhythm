5e61b4412bf64035eb75a8a021483c321696e975
initcatcher <initcatcher@gmail.com>
2025-08-30 09:12:52 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 47.22 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    const answer = input[0].split('').map((ch)=> {
+        if('a' <= ch){
+            return ch.toUpperCase()
+        }else {
+            return ch.toLowerCase()
+        }
+    }).join('')
+    
+    console.log(answer)
+});
