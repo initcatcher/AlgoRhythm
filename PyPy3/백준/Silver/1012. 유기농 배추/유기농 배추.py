4b07b201165c9b85bb10b9d790cb152fedf74154
initcatcher <initcatcher@gmail.com>
2025-09-12 09:31:49 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 114852 KB -BaekjoonHub

--- PyPy3/백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ PyPy3/백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -3,34 +3,31 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    M, N, K = map(int, input().split())
+    M,N,K = map(int,input().split())
 
     grid = [[0] * M for _ in range(N)]
     check = [[False] * M for _ in range(N)]
 
     for _ in range(K):
-        x, y = map(int, input().split())
+        x,y = map(int,input().split())
         grid[y][x] = 1
     
     ans = 0
     for y in range(N):
         for x in range(M):
             if not check[y][x] and grid[y][x] == 1:
+
                 ans += 1
                 dq = deque()
-                dq.append((y, x))
+                dq.append((y,x))
                 check[y][x] = True
-                
                 while dq:
-                    cy, cx = dq.popleft()  # 변수명 변경으로 혼동 방지
-                    
-                    for dy, dx in [[0,1], [-1,0], [0,-1], [1,0]]:
-                        next_x, next_y = cx + dx, cy + dy
-                        if 0 <= next_x < M and 0 <= next_y < N and not check[next_y][next_x]:
+                    cy,cx = dq.popleft()
+
+                    for dy,dx in [[0,1],[-1,0],[0,-1],[1,0]]:
+                        next_x, next_y = cx+dx, cy+ dy
+                        if 0<=next_x<M and 0<=next_y<N and not check[next_y][next_x]:
                             if grid[next_y][next_x]:
                                 check[next_y][next_x] = True
-                                dq.append((next_y, next_x))
-                
-                # 디버깅 출력 제거!
-    
-    print(ans)  # 오직 이것만 출력
+                                dq.append((next_y,next_x))
+    print(ans)
